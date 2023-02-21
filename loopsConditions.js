d8e4fcd7dd45d189ff77f51af415ddf9ce5ec47c
iAntonova <antonovava@gmail.com>
2023-02-21 07:54:08 +0200

    if and while

--- loopsConditions.js
+++ loopsConditions.js
@@ -1,3 +1,20 @@
 /*
 Loops and Conditions in JS
 */
+//declear boolean const variable
+const flag = true
+// if condition
+if (!flag) {
+    console.log("Condition satisfied")
+}
+else {
+    console.log(flag)
+    console.log("Condition not satisfied")
+}
+
+// while loop
+let i = 0
+while (i < 10) {
+    i++
+    console.log(i)
+}
